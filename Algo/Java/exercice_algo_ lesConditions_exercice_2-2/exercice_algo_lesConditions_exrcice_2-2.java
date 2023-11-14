6745aac3b29a03ca58c1124a95fc08768c484e92
BRETON Frédéric <FBreton@arfp.asso.fr>
2023-11-14 16:01:24 +0100

    update exercice java algo

---
+++ Algo/Java/exercice_algo_ lesConditions_exercice_2-2/exercice_algo_lesConditions_exrcice_2-2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String args[]) {
+        
+        int nombreUtilisateur1;
+        int nombreUtilisateur2;
+        
+        Scanner nombre = new Scanner(System.in);
+        System.out.println("Saisir un premier nombre : ");
+        nombreUtilisateur1 = nombre.nextInt();
+        
+        System.out.println("Saisir un deuxième nombre : ");
+        nombreUtilisateur2 = nombre.nextInt();
+        
+        if(nombreUtilisateur1 == 0 || nombreUtilisateur2 == 0){
+            System.out.println("Le produit de ces 2 nombres est nul vous avez saisie au moins un nombre égal à zéro");
+        }else if(nombreUtilisateur1 > 0 && nombreUtilisateur2 < 0){
+            System.out.println("Le produit de ces 2 nombres est négatif");
+        }else if(nombreUtilisateur1 < 0 && nombreUtilisateur2 > 0){
+            System.out.println("Le produit de ces 2 nombres est négatif");
+        }else if(nombreUtilisateur1 < 0 && nombreUtilisateur2 < 0){
+            System.out.println("Le produit de ces 2 nombres est positif");
+        }else if(nombreUtilisateur1 > 0 && nombreUtilisateur2 > 0){
+            System.out.println("Le produit de ces 2 nombres est positif");
+        }
+    }
+}

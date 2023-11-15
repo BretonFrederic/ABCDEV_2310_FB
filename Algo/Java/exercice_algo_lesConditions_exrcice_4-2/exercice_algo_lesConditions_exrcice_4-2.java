01cfbad6d7f780418d5fff197822c732eee6e7db
BRETON Frédéric <FBreton@arfp.asso.fr>
2023-11-15 14:49:57 +0100

    exercice 4.2

---
+++ Algo/Java/exercice_algo_lesConditions_exrcice_4-2/exercice_algo_lesConditions_exrcice_4-2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String args[]) {
+        
+        /* VARIABLES */
+        
+        int heure;
+        int minute;
+        int seconde;
+        
+        /* DEBUT PROGRAMME */
+        
+        Scanner utilisateur = new Scanner(System.in);
+        
+        System.out.println("Saisir l'heure : ");
+        heure = utilisateur.nextInt();
+        System.out.println("Saisir les minutes : ");
+        minute = utilisateur.nextInt();
+        System.out.println("Saisir les secondes : ");
+        seconde = utilisateur.nextInt();
+        
+        if(seconde == 59){
+            seconde = 0;
+	        minute = minute + 1;
+        }else if(seconde < 59){
+            seconde = seconde + 1;
+        }
+        
+        if(minute >= 59){
+            minute = 0;
+	        heure = heure + 1;
+        }
+        
+        System.out.println("Dans une minute il sera " + heure + " heure(s) " + minute + " minute(s) et " + seconde + "seconde(s)");
+        
+        /* FIN PROGRAMME */
+    }
+}
---
+++ Algo/Java/exercice_algo_lesConditions_exrcice_4/exercice_algo_lesConditions_exrcice_4.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+        
+        /* VARIABLES */
+        
+        int heure;
+        int minute;
+        
+        /* DEBUT PROGRAMME */
+        
+        Scanner utilisateur = new Scanner(System.in);
+        System.out.println("Saisir l'heure : ");
+        heure = utilisateur.nextInt();
+        System.out.println("Saisir les minutes : ");
+        minute = utilisateur.nextInt();
+        
+        if(minute == 59){
+            minute = 0;
+	        heure = heure + 1;
+        }else if(minute < 59){
+            minute = minute + 1;
+        }
+        
+        System.out.println("Dans une minute il sera " + heure + " heures " + minute);
+        
+        /* FIN PROGRAMME */
+    }
+}

31bd0e0a5c05b7ef9c92b720c1d3f9a5b8e732f5
FBretonABCDEV <fred.eks@orange.fr>
2023-11-16 22:10:18 +0100

    j'ai plus de cheveux mais j'ai réussi :p

--- Algo/Java/exercice_algo_lesConditions_exrcice_5/exercice_algo_lesConditions_exrcice_5.java
+++ Algo/Java/exercice_algo_lesConditions_exrcice_5/exercice_algo_lesConditions_exrcice_5.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
-import java.text.DecimalFormat;
 
 public class MyClass {
     public static void main(String args[]) {
+        /*****************************************************/
+        /* SIGNEZ LA PETITION POUR INTERDIRE LES IMPRIMANTES */
+        /*****************************************************/
+        
         /* VARIABLES */
         
         int photocopie = 0;
         double facture = 0.0;
+        double arrondi = 0.0;
+        double factureFinale = 0.0;
         
         /* DEBUT PROGRAMME */
         
@@ -17,18 +22,20 @@ public class MyClass {
         
         
         for(int i = 0; i < photocopie; i++){
-        	if(i <= 10){
-        	    facture = facture + 0.1;
-        	}else if(i >= 11 && i <= 20){
-        	    facture = facture + 0.09;
-        	}else if(i > 20){
-        	    facture = facture + 0.08;
+        	if(i < 10){
+        	    facture = facture + 10;
+        	}else if(i > 9 && i < 20){
+        	    facture = facture + 9;
+        	}else if(i > 19){
+        	    facture = facture + 8;
         	}
         }
         
-        DecimalFormat decimal = new DecimalFormat("0.00");
+        arrondi = Math.round(facture);
+        factureFinale = arrondi/100;
         
-        System.out.println("Le montant de la facture pour " + photocopie + " photocopie(s) est de :" + decimal.format(facture) + " Euro(s) ");
+        System.out.println("Le montant de la facture pour " +  photocopie + " photocopie(s) est de : " + factureFinale + " Euro(s)");
+        //System.out.printf("Le montant de la facture pour %d photocopie(s) est de : %.2f Euro(s)\n", photocopie, facture);
         
         /* FIN PROGRAMME */
     }

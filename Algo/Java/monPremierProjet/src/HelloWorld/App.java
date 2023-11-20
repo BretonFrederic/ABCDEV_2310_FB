40b621cc1ea7bc95aa1808f8fec911c991003079
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2023-11-20 16:56:57 +0100

    exercice 7 update

---
+++ Algo/Java/monPremierProjet/src/HelloWorld/App.java
@@ -0,0 +1,35 @@
+package HelloWorld;
+
+public class App {
+
+	public static void main(String[] args) {
+		/*
+		 * Exemple 
+		 * de 
+		 * code 
+		 * Java
+		 */
+		// Déclartion d'une variable de type chaine de caractère
+		String maVariable;
+		char letter = 'm';
+		
+		// Déclaration variable type entier
+		int nombre; // 32 bits
+		nombre = 5;
+		float nombre2; // 32 bits
+		nombre2 = 2.5f;
+		double nombre3; // 64 bits
+		nombre3 = 3.25;
+		long nombre4; //64 bits
+		nombre4 = 15412;
+		
+		int resultat = 3+2;
+		
+		// Déclaration de type boolean
+		boolean choix;
+		choix = false;
+		
+;		maVariable = "les codeurs et les codeuses";
+		System.out.println("Salut " + maVariable);
+	}
+}

419da171d2934c1c99e6fde8a53a435c134ff82f
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2023-11-29 08:56:43 +0100

    amélioration exercice 3.1 jeu de la fourchette

--- Algo/Java/Algo05_ABC_ExercicesAlgo/src/partie_3_jeu_de_la_fourchette/App.java
+++ Algo/Java/Algo05_ABC_ExercicesAlgo/src/partie_3_jeu_de_la_fourchette/App.java
@@ -17,17 +17,19 @@ public class App {
 		Scanner saisie = new Scanner(System.in);
 
 		nombreMystere = (int)Math.round(Math.random()*100);
-		//System.out.println("Saisir un nombre entier entre 0 et 100 : ");
+		
+		System.out.println("Saisir un nombre entier entre 0 et 100 : ");
 
 		do {
-			System.out.println("Saisir un nombre entier entre 0 et 100 : ");
 			nombreJoueur = saisie.nextInt();
 			nombreEssai++;
 			if(nombreJoueur == nombreMystere) {
 					System.out.println("Bravo, vous avez trouvez le nombre mystère en " + nombreEssai + " essai(s).");
 				}else if(nombreJoueur < nombreMystere) {
+					System.out.println("C'est plus ! ");
 					System.out.println("Le nombre mystère est compris entre " + nombreJoueur + " est " + 100);
 				}else if(nombreJoueur > nombreMystere) {
+					System.out.println("C'est moins ! ");
 					System.out.println("Le nombre mystère est compris entre " + 0 + " est " + nombreJoueur);
 			}
 		}while(nombreJoueur != nombreMystere);

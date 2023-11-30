468ea22dbca5c4374fb0bcdd704f25648b064baa
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2023-11-30 08:29:46 +0100

    exercice 3.3 trouver nombre dans tableau

--- Algo/Java/Algo05_ABC_ExercicesAlgo/src/partie_3_recherche_nombre_dans_tableau/App.java
+++ Algo/Java/Algo05_ABC_ExercicesAlgo/src/partie_3_recherche_nombre_dans_tableau/App.java
@@ -7,8 +7,9 @@ public class App {
 		
 		/* VALEURS */
 
-		int nombreAleatoire;
-		int liste[]= new int[10];
+		//int nombreAleatoire;
+		//int liste[]= new int[10];
+		int liste[]= new int[]{25,18,5,47,32,22,15,97,54,72};
 		int nombreUtilisateur;
 		int devinerNombre = 0;
 		int indice;
@@ -20,11 +21,15 @@ public class App {
 		System.out.println("Saisir un nombre entier entre 0 et 100 : ");
 		nombreUtilisateur = saisie.nextInt();
 		
+		/*Générer nombre aléatoire
+		
 		for(int i = 0; i < 10; i++) {
 			nombreAleatoire = (int)Math.round(Math.random()*100);
 			liste[i] = nombreAleatoire;
-		}
+		}*/
 
+		/*Tri nombre aléatoire ordre croissant
+		
 		for(int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
 				if(liste[i] < liste[j]) {
@@ -33,8 +38,10 @@ public class App {
 					liste[j] = temp;
 				}
 			}
-		}
+		}*/
 
+		//Cherche le nombre de l'utilisateur si présent dans tableau et renvoie son indice 
+		
 		for(int i = 0; i < 10; i++) {
 			if(liste[i] == nombreUtilisateur) {
 				indice = i+1;

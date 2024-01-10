61979861cbf2c878661bb2cdae055676ac37f7db
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-01-10 15:58:40 +0100

    exemple UML

--- Objet/Java/Base_objet/src/exercice_bouteille/Bouteille.java
+++ Objet/Java/Base_objet/src/exercice_bouteille/Bouteille.java
@@ -2,4 +2,31 @@ package exercice_bouteille;
 
 public class Bouteille {
 
+	// Attributs
+	private String marque = "nom";
+	private String parfum = "nom";
+	private boolean gazeux = false;
+	private int volumeMax = 1000;
+	private int volumeReel = 500;
+	private boolean estFermer = true;
+	
+	// Méthodes
+	public void ouvrir() {
+		if(estFermer) {
+			estFermer = false;
+		}
+	}
+	
+	public void fermer() {
+		if(!estFermer) {
+			estFermer = true;
+		}
+	}
+	
+	public void vider(int _quantite) {
+		int volumeReel = volumeReel - _quantite;
+		if(!estFermer && volumeReel >= 0) {
+			estFermer = true;
+		}
+	}
 }

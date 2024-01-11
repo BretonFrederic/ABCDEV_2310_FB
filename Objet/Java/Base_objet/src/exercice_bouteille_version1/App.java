933887f3fb6fb782b1cfdee93c813ad2bd59f73d
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-01-11 16:32:26 +0100

    base objet exercice bouteille_version1

--- Objet/Java/Base_objet/src/exercice_bouteille_version1/App.java
+++ Objet/Java/Base_objet/src/exercice_bouteille_version1/App.java
@@ -8,13 +8,25 @@ public class App {
 		double capacite = 2.0;
 		double contenance = 1.2;
 		boolean ouverte = false;
-		double quantite = 0.8;
+		double quantite = 0.4;
 		
 		/* DEBUT PROGRAMME */
 		
 		Bottle bouteille1 = new Bottle();
 		
+		boolean testOuverture = bouteille1.ouvrir();
+		boolean testRemplir = bouteille1.remplir(quantite);
+		boolean vider = bouteille1.vider(quantite);
+		boolean testRemplirTout = bouteille1.remplirTout();
+		boolean testViderTout = bouteille1.viderTout();
+		
 		Bottle bouteille2 = new Bottle(nom, capacite, contenance, ouverte);
+		
+		testOuverture = bouteille2.ouvrir();
+		testRemplir = bouteille2.remplir(quantite);
+		vider = bouteille2.vider(quantite);
+		testRemplirTout = bouteille2.remplirTout();
+		testViderTout = bouteille2.viderTout();
 		
 		/* FIN PROGRAMME */
 	}

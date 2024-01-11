18190a3e2f7ff2a5d241d0703af85de4cf7f17cd
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-01-11 16:47:50 +0100

    base objet exercice bouteille_version1

--- Objet/Java/Base_objet/src/exercice_bouteille_version1/App.java
+++ Objet/Java/Base_objet/src/exercice_bouteille_version1/App.java
@@ -14,19 +14,31 @@ public class App {
 		
 		Bottle bouteille1 = new Bottle();
 		
+		// Appelle des méthodes pour une instance de la classe Bottle qui ne prend pas de paramètre
 		boolean testOuverture = bouteille1.ouvrir();
 		boolean testRemplir = bouteille1.remplir(quantite);
 		boolean vider = bouteille1.vider(quantite);
 		boolean testRemplirTout = bouteille1.remplirTout();
 		boolean testViderTout = bouteille1.viderTout();
+		boolean testFermer = bouteille1.fermer();
+		
+		// Réinitialisation des booléens des tests méthodes
+		testOuverture = false;
+		testRemplir = false;
+		vider = false;
+		testRemplirTout = false;
+		testViderTout = false;
+		testFermer = false;
 		
 		Bottle bouteille2 = new Bottle(nom, capacite, contenance, ouverte);
 		
+		// Appelle des méthodes pour une instance de la classe Bottle qui prend des paramètres
 		testOuverture = bouteille2.ouvrir();
 		testRemplir = bouteille2.remplir(quantite);
 		vider = bouteille2.vider(quantite);
 		testRemplirTout = bouteille2.remplirTout();
 		testViderTout = bouteille2.viderTout();
+		testFermer = bouteille1.fermer();
 		
 		/* FIN PROGRAMME */
 	}

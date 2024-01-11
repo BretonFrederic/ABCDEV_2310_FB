88540820f9d0429cc49c651fc6c4c6a79e06728e
FBretonABCDEV <fred.eks@orange.fr>
2024-01-11 07:46:31 +0100

    exercice objet bouteille

--- Objet/Java/Base_objet/src/exercice_bouteille/App.java
+++ Objet/Java/Base_objet/src/exercice_bouteille/App.java
@@ -1,14 +1,69 @@
 package exercice_bouteille;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		
+		/* VARIABLES */
+		String nom = "Evian";
+		double capacite = 2.0;
+		double contenance = 1.2;
+		boolean ouverte = false;
+		double quantite;
+		int action;
+		String quitter="y";
+		
+		/* DEBUT PROGRAMME */
+		Scanner saisie = new Scanner(System.in);
+		
 		Bouteille vittel = new Bouteille();
 		
-		boolean testBouchon = ouvrir();
-		
-	}
+		Bouteille evian = new Bouteille(nom, capacite, contenance, ouverte);
+		System.out.print("La bouteille contient : " + contenance + " Litres\n");
+		while(!quitter.equals("q")) {
+			System.out.print("Saisir une action : "
+					+ "\n\t1 fermer"
+					+ "\n\t2 ouvrir"
+					+ "\n\t3 vider"
+					+ "\n\t4 vider tout"
+					+ "\n\t5 remplir"
+					+ "\n\t6 remplir tout\n");
+			action = saisie.nextInt();
+			switch(action) {
+				case 1:
+					boolean testFermer = evian.fermer();
+					break;
+				case 2:
+					boolean testOuvrir = evian.ouvrir();
+					break;
+				case 3:
+					System.out.print("Saisir une quantité à vider : ");
+					quantite = saisie.nextDouble();
+					boolean testVider = evian.vider(quantite);
+					break;
+				case 4:
+					boolean testViderTout = evian.viderTout();
+					break;
+				case 5:
+					System.out.print("Saisir une quantité à remplir : ");
+					quantite = saisie.nextDouble();
+					boolean testRemplir = evian.remplir(quantite);
+					break;
+				case 6:
+					boolean testRemplirTout = evian.remplirTout();
+					break;
+				default:
+					System.out.print("Vous devez choisir entre 1 et 6");
+					break;
+			}
+			System.out.print("Saisir q pour quitter ou n'importe quelle autre touche pour continuer : ");
+			quitter = saisie.next();
+		}
+		saisie.close();
+		/* FIN PROGRAMME */
 
+	}
 	
 }
--- Objet/Java/Base_objet/src/exercice_bouteille/Bouteille.java
+++ Objet/Java/Base_objet/src/exercice_bouteille/Bouteille.java
@@ -3,15 +3,18 @@ package exercice_bouteille;
 public class Bouteille {
 
 	// Attributs
-	private String nom = "nom";
-	private double capaciteEnL = 1.5;
-	private double contenanceEnL = 0.5;
-	private boolean estOuverte = false;
+	private String nom;
+	private final double capaciteEnL;
+	private double contenanceEnL;
+	private boolean estOuverte;
 	
 	// Méthodes
 	public boolean ouvrir() {
 		if(!estOuverte) {
 			estOuverte = true;
+			System.out.print("La bouteille est ouverte\n");
+		}else if(estOuverte) {
+			System.out.print("Déjà ouverte\n");
 		}
 		return estOuverte;
 	}
@@ -19,55 +22,78 @@ public class Bouteille {
 	public boolean fermer() {
 		if(estOuverte) {
 			estOuverte = false;
+			System.out.print("La bouteille est fermée\n");
+		}else if(!estOuverte) {
+			System.out.print("Déjà fermée\n");
 		}
 		return estOuverte;
 	}
 	
 	public boolean remplirTout() {
-		contenanceEnL = capaciteEnL;
-		return true;
+		boolean remplirTout;
+		if(contenanceEnL < capaciteEnL && estOuverte) {
+			contenanceEnL = capaciteEnL;
+			System.out.print("La bouteille est pleine, elle contient : " + contenanceEnL + " Litres\n");
+			remplirTout = true;
+		}else {
+			System.out.print("Déjà remplie ou bouteille fermée\n");
+			remplirTout = false;
+		}
+		return remplirTout;
 	}
 	
 	public boolean remplir(double quantiteEnL) {
 		boolean remplir;
-		if(quantiteEnL <= capaciteEnL - contenanceEnL) {
+		if(quantiteEnL <= capaciteEnL - contenanceEnL && estOuverte) {
 			contenanceEnL += quantiteEnL;
 			remplir = true;
+			System.out.print("La bouteille contient : " + contenanceEnL + " Litres\n");
 		}else {
+			System.out.print("Quantité à remplir plus grande que la capacité ou bouteille fermée\n");
 			remplir = false;
 		}
 		return remplir;
 	}
 	
 	public boolean viderTout() {
-		contenanceEnL = 0;
-		return true;
+		boolean viderTout;
+		if(contenanceEnL > 0 && estOuverte) {
+			contenanceEnL = 0;
+			System.out.print("La bouteille est vide\n");
+			viderTout = true;
+		}else {
+			System.out.print("Déjà vide ou bouteille fermée\n");
+			viderTout = false;
+		}
+		return viderTout;
 	}
 	
 	public boolean vider(double quantiteEnL) {
 		boolean vider;
-		if(contenanceEnL >= quantiteEnL) {
+		if(contenanceEnL >= quantiteEnL && estOuverte) {
 			contenanceEnL -= quantiteEnL;
+			System.out.print("La bouteille contient : " + contenanceEnL + " Litres\n");
 			vider = true;
 		}else {
+			System.out.print("Quantité à vider plus grande que la contenance ou bouteille fermée\n");
 			vider = false;
 		}
 		return vider;
 	}
 	
 	// Constructeur
-	Bouteille(){
-		this.nom = "nom";
-		this.capaciteEnL = 1.5;
-		this.contenanceEnL = 0.5;
-		this.estOuverte = false;
+	public Bouteille(){
+		nom = "Vittel";
+		capaciteEnL = 1.5;
+		contenanceEnL = 0.5;
+		estOuverte = false;
 	}
 	
-	Bouteille(String _nom, double _capaciteEnL, double _contenanceEnL, boolean _estOuverte){
-		this.nom = _nom;
-		this.capaciteEnL = _capaciteEnL;
-		this.contenanceEnL = _contenanceEnL;
-		this.estOuverte = _estOuverte;
+	public Bouteille(String _nom, final double _capaciteEnL, double _contenanceEnL, boolean _estOuverte){
+		nom = _nom;
+		capaciteEnL = _capaciteEnL;
+		contenanceEnL = _contenanceEnL;
+		estOuverte = _estOuverte;
 	}
 	
 }

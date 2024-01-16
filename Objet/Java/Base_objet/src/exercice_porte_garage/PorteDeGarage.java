e5096d673932be98622316d47f8f11e9b3794b24
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-01-16 14:54:15 +0100

    POO en java

---
+++ Objet/Java/Base_objet/src/exercice_porte_garage/PorteDeGarage.java
@@ -0,0 +1,32 @@
+package exercice_porte_garage;
+
+public class PorteDeGarage {
+	
+	//Attribut
+	private String systemeOuverture;
+	private final int positionMax;
+	private final int positionMin;
+	private int pourcentageouverture;
+	private boolean estOuverte;
+	private boolean estverrouillee;
+	
+	//Méthode
+	public boolean Ouvrir() {
+		
+	}
+	
+	public boolean Fermer() {
+		
+	}
+	
+	public boolean Ouvrir(int _pourcentageOuverture) {
+		
+	}
+	
+	public boolean Fermer(int _pourcentageOuverture) {
+		
+	}
+	
+	//Constructeur
+	
+}

94907bbdc74b8f678599037c3db04d9742de5822
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-01-17 09:30:07 +0100

    exercice porte de garage objet

--- Objet/Java/Base_objet/src/exercice_porte_garage/App.java
+++ Objet/Java/Base_objet/src/exercice_porte_garage/App.java
@@ -3,8 +3,47 @@ package exercice_porte_garage;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//VARIABLES
+		String systemeOuverture = "coulissante";
+		int positionMax = 100;
+		int positionMin = 0;
+		int pourcentageOuverture = 50;
+		boolean estOuverte = true;
+		boolean estVerrouillee = false;
+		
+		//DEBUT PROGRAMME
+		
+		PorteDeGarage porteJeune = new PorteDeGarage();
+		PorteDeGarage porteClignancourt = new PorteDeGarage(systemeOuverture, positionMax, positionMin, pourcentageOuverture, estOuverte, estVerrouillee);
+		
+		//Appelle méthode porteJeune
+		boolean deverrouiller = porteJeune.Deverrouiller();
+		boolean ouvrirTout = porteJeune.OuvrirTout();
+		boolean ouvrir = porteJeune.Ouvrir(pourcentageOuverture);
+		boolean fermer = porteJeune.Fermer(pourcentageOuverture);
+		boolean fermerTout = porteJeune.FermerTout();
+		boolean verrouiller = porteJeune.Verrouiller();
+		
+		
+		//Réninitialisation des boolean
+		ouvrirTout = false;
+		fermerTout = false;
+		ouvrir = false;
+		fermer = false;
+		verrouiller = false;
+		deverrouiller = false;
+		
+		//Appelle méthode porteClignancourt
+		deverrouiller = porteClignancourt.Deverrouiller();
+		ouvrirTout = porteClignancourt.OuvrirTout();
+		ouvrir = porteClignancourt.Ouvrir(pourcentageOuverture);
+		fermer = porteClignancourt.Fermer(pourcentageOuverture);
+		fermerTout = porteClignancourt.FermerTout();
+		verrouiller = porteClignancourt.Verrouiller();
+		
+		
+		//FIN PROGRAMME
 	}
 
 }
--- Objet/Java/Base_objet/src/exercice_porte_garage/PorteDeGarage.java
+++ Objet/Java/Base_objet/src/exercice_porte_garage/PorteDeGarage.java
@@ -6,27 +6,92 @@ public class PorteDeGarage {
 	private String systemeOuverture;
 	private final int positionMax;
 	private final int positionMin;
-	private int pourcentageouverture;
+	private int pourcentageOuverture;
 	private boolean estOuverte;
-	private boolean estverrouillee;
+	private boolean estVerrouillee;
 	
 	//Méthode
-	public boolean Ouvrir() {
-		
+	public boolean OuvrirTout() {
+		if(!estOuverte && !estVerrouillee) {
+			pourcentageOuverture = positionMax;
+			estOuverte = true;
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
-	public boolean Fermer() {
-		
+	public boolean FermerTout() {
+		if(estOuverte && !estVerrouillee) {
+			pourcentageOuverture = positionMin;
+			estOuverte = false;
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public boolean Verrouiller() {
+		if(!estOuverte && !estVerrouillee) {
+			estVerrouillee = true;
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public boolean Deverrouiller() {
+		if(!estOuverte && estVerrouillee) {
+			estVerrouillee = false;
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
 	public boolean Ouvrir(int _pourcentageOuverture) {
-		
+		if(_pourcentageOuverture > pourcentageOuverture &&_pourcentageOuverture < positionMax && !estVerrouillee) {
+			pourcentageOuverture = _pourcentageOuverture;
+			if(pourcentageOuverture == 0) {
+				estOuverte = false;
+				}else {
+					estOuverte = true;
+				}
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
 	public boolean Fermer(int _pourcentageOuverture) {
-		
+		if(_pourcentageOuverture < pourcentageOuverture &&_pourcentageOuverture >= positionMin && !estVerrouillee) {
+			pourcentageOuverture = _pourcentageOuverture;
+			if(pourcentageOuverture == 0) {
+			estOuverte = false;
+			}else {
+				estOuverte = true;
+			}
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
 	//Constructeur
-	
+	public PorteDeGarage() {
+		systemeOuverture = "basculante";
+		positionMax = 100;
+		positionMin = 0;
+		pourcentageOuverture = 0;
+		estOuverte = false;
+		estVerrouillee = true;
+	}
+	public PorteDeGarage(String _systemeOuverture, int _positionMax, int _positionMin, int _pourcentageOuverture, boolean _estOuverte, boolean _estVerrouillee) {
+		systemeOuverture = _systemeOuverture;
+		positionMax = _positionMax;
+		positionMin = _positionMin;
+		pourcentageOuverture = _pourcentageOuverture;
+		estOuverte = _estOuverte;
+		estVerrouillee = _estVerrouillee;
+	}
 }

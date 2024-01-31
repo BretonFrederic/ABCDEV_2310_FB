cb3282b8968a769574df223a36fb53917c44be34
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-01-31 16:59:01 +0100

    update

--- Objet/Java/Base_objet/src/exercice_geometry/TestCercle.java
+++ Objet/Java/Base_objet/src/exercice_geometry/TestCercle.java
@@ -5,8 +5,8 @@ public class TestCercle {
 	public static void main(String[] args) {
 		
 		//création d'un cercle
-		Point centreCercle1 = new Point(-2, 0);
-		Cercle cercle1 = new Cercle(centreCercle1, 10);
+		Point centreCercle1 = new Point(1, 1);
+		Cercle cercle1 = new Cercle(centreCercle1, 2);
 		
 		//création d'un point
 		Point point1 = new Point(8, 0);

a61d0250244a1c4c6842d9b0531f836bb9a26497
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-02-05 14:54:49 +0100

    robot

---
+++ Objet/Java/Base_objet/src/exercice_le_robot/Detecteur.java
@@ -0,0 +1,40 @@
+package exercice_le_robot;
+
+public class Detecteur {
+	//attributs
+	Position position;
+	double rayon;
+	
+	//constructeurs
+	public Detecteur(Position _position, double _rayon) {
+		this.position = _position;
+		this.rayon = _rayon;
+	}
+	
+	//méthode
+	public double perimetre() {
+		double perimetre = 2* Math.PI * this.rayon;
+		return perimetre;
+	}
+	
+	public double surface() {
+		double surface = Math.PI * Math.pow(this.rayon, 2);
+		return surface;
+	}
+	
+	public boolean Appartenance(Position _position) {
+		//pythagore
+		double distancePoint = Math.sqrt(Math.abs(Math.pow(((_position.getPositionX() - this.position.getPositionX())), 2)) + Math.abs(Math.pow(((_position.getPositionY() - this.position.getPositionY())), 2)));
+		if(rayon == distancePoint) {
+			System.out.println("Le scanner a détecté de la roche lunaire.");
+			return true;
+		}else {
+			System.out.println("Le scanner n'a pas détecté de roche lunaire.");
+			return false;
+		}
+	}
+	
+	public void Afficher() {
+		System.out.println("Le périmètre du cercle est : "+this.perimetre()+"\nLa surface du disque est : "+this.surface()+" ²");
+	}
+}
---
+++ Objet/Java/Base_objet/src/exercice_le_robot/Point.java
@@ -0,0 +1,23 @@
+package exercice_le_robot;
+
+public class Point {
+	
+	//variables d'instance
+	private double abscisse;
+	private double ordonnee;
+	
+	//constructeurs
+	public Point(double _abscisse, double _ordonnee) {
+		this.abscisse = _abscisse;
+		this.ordonnee = _ordonnee;
+	}
+	
+	//getters
+	public double getAbscisse(){
+		return this.abscisse;
+	}
+	
+	public double getOrdonnee(){
+		return this.ordonnee;
+	}
+}

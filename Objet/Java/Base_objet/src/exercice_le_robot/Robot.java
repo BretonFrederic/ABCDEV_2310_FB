5c9802f3de4368c161ee6673b380583875305c38
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-02-05 09:07:42 +0100

    robot

---
+++ Objet/Java/Base_objet/src/exercice_le_robot/Robot.java
@@ -0,0 +1,14 @@
+package exercice_le_robot;
+
+public class Robot {
+	
+	//attributs
+	private String nom;
+	private Position position;
+	private boolean batterie;
+	private boolean moteur;
+	private boolean chassi;
+	private boolean brasArticule;
+	private boolean roues;
+	private Roche reserveEchantillon;
+}

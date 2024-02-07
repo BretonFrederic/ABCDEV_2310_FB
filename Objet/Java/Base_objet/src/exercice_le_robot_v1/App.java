e81829766b5299904b9960bbce761ed7d0bb7126
FBretonABCDEV <fred.eks@orange.fr>
2024-02-07 22:54:08 +0100

    robot version 1

--- Objet/Java/Base_objet/src/exercice_le_robot_v1/App.java
+++ Objet/Java/Base_objet/src/exercice_le_robot_v1/App.java
@@ -4,16 +4,23 @@ public class App {
 
 	public static void main(String[] args) {
 
+		//Variables
+		int distance = 7;
+		
+		//Debut programme
+		
 		Secteur secteur1 = new Secteur();
 		Robot r2d2 = new Robot("r2d2");
-		r2d2.Avancer(Direction.DROITE);
-		r2d2.Avancer(Direction.DROITE);
-		r2d2.Avancer(Direction.DROITE);
-		r2d2.Avancer(Direction.DROITE);
-		//r2d2.Avancer(Direction.DROITE);
-		//r2d2.Avancer(Direction.DROITE);
-		//r2d2.Avancer(Direction.DROITE);
+		
+		while(distance > 0) {
+			r2d2.Avancer(Direction.DROITE);
+			distance--;
+		}
+		
 		r2d2.Detecter(secteur1);
 		r2d2.Recolter(secteur1);
+		r2d2.AfficherInfo();
+		
+		//Fin programme
 	}
 }
--- Objet/Java/Base_objet/src/exercice_le_robot_v1/Robot.java
+++ Objet/Java/Base_objet/src/exercice_le_robot_v1/Robot.java
@@ -6,6 +6,7 @@ public class Robot {
 	private String nom;
 	private Position position;
 	private int stockage;
+	private int batterie;
 	private final double rayonDetection;
 	
 	//Constructeurs
@@ -13,48 +14,64 @@ public class Robot {
 		this.nom = _nom;
 		this.position = new Position(10, 10);
 		this.stockage = 0;
-		rayonDetection = 2;
+		this.batterie = 100;
+		this.rayonDetection = 2;
 	}
 	
 	//Méthodes
 	public void Avancer(Direction _direction) {
 		boolean horsLimiteX = this.position.getPositionX()== 20 || this.position.getPositionX()== 0;
 		boolean horsLimiteY = this.position.getPositionY()== 20 || this.position.getPositionY()== 0;
-		switch(_direction) {
-		case HAUT:
-			if(!horsLimiteY) {
-				this.position.setPositionY(this.position.getPositionY()+1);
+		if(batterie > 0) {
+			switch(_direction) {
+			case HAUT:
+				if(!horsLimiteY) {
+					this.batterie -= 2;
+					this.position.setPositionY(this.position.getPositionY()+1);
+				}
+				break;
+			case BAS:
+				if(!horsLimiteY) {
+					this.batterie -= 2;
+					this.position.setPositionY(this.position.getPositionY()-1);
+				}
+			case GAUCHE:
+				if(!horsLimiteX) {
+					this.batterie -= 2;
+				this.position.setPositionX(this.position.getPositionX()-1);
+				}
+				break;
+			case DROITE:
+				if(!horsLimiteX) {
+					this.batterie -= 2;
+					this.position.setPositionX(this.position.getPositionX()+1);
+				}
+				break;
 			}
-			break;
-		case BAS:
-			if(!horsLimiteY) {
-				this.position.setPositionY(this.position.getPositionY()-1);
-			}
-		case GAUCHE:
-			if(!horsLimiteX) {
-			this.position.setPositionX(this.position.getPositionX()-1);
-			}
-			break;
-		case DROITE:
-			if(!horsLimiteX) {
-				this.position.setPositionX(this.position.getPositionX()+1);
-			}
-			break;
 		}
+		
 	}
 	
 	public void Detecter(Secteur _secteur) {
+		int compteur = 0;
+		this.batterie -= 2;
 		for(int i = 0; i <= _secteur.getTableauRoche().length-1; i++) {
 			Position posRoche = _secteur.getRochePosition(i);
 			//pythagore
 			double distancePoint = Math.sqrt(Math.abs(Math.pow(((posRoche.getPositionX() - this.position.getPositionX())), 2)) + Math.abs(Math.pow(((posRoche.getPositionY() - this.position.getPositionY())), 2)));
 			if(rayonDetection >= distancePoint) {
-				System.out.println("\nLe scanner a détecté de la roche lunaire.\n");
+				compteur++;
 			}
+		}
+		if(compteur > 0) {
+			System.out.println("\nLe scanner a détecté de la roche lunaire.\n");
+		}else {
+			System.out.println("\nLe scanner n'a rien détecté.\n");
 		}
 	}
 	
 	public void Recolter(Secteur _secteur) {
+		this.batterie -= 2;
 		for(int i = 0; i <= _secteur.getTableauRoche().length-1; i++) {
 			Position posRoche = _secteur.getRochePosition(i);
 			//pythagore
@@ -62,8 +79,12 @@ public class Robot {
 			if(rayonDetection >= distancePoint) {
 				this.stockage += _secteur.getRochePoids(i);
 				_secteur.setRochePosition(i);
-				System.out.printf("\nLe robot a récolté : %d gramme(s) de roche lunaire", _secteur.getRochePoids(i));
+				System.out.printf("\nLe robot a récolté : %d gramme(s) de roche lunaire\n", _secteur.getRochePoids(i));
 			}
 		}
+	}
+	
+	public void AfficherInfo() {
+		System.out.printf("\nNom : %s\nPosition : x = %d, y = %d\nStockage : %d\nBatterie : %d\n", this.nom, this.position.getPositionX(), this.position.getPositionY(), this.stockage, this.batterie);
 	}
 }

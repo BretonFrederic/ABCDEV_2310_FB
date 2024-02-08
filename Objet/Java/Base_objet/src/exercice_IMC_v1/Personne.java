780d498f3aade0991bdf3596aa00dcc3c7e230ab
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-02-08 16:57:30 +0100

    exercice IMC

---
+++ Objet/Java/Base_objet/src/exercice_IMC_v1/Personne.java
@@ -0,0 +1,19 @@
+package exercice_IMC_v1;
+
+public class Personne {
+	
+	//Attributs
+	private double taille;
+	private double poids;
+	private int age;
+	
+	//Constructeur
+	public Personne(double _taille, double _poids, int _age) {
+		taille = _taille;
+		poids = _poids;
+		age = _age;
+	}
+	
+	//Méthodes
+	
+}

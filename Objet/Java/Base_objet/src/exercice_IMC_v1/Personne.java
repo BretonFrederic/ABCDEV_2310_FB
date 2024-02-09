7f448c0939539742b5098c446a2e419bf5649ce4
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-02-09 11:56:12 +0100

    imc

--- Objet/Java/Base_objet/src/exercice_IMC_v1/Personne.java
+++ Objet/Java/Base_objet/src/exercice_IMC_v1/Personne.java
@@ -19,8 +19,22 @@ public class Personne {
 		return this.poids/Math.pow(this.taille, 2);
 	}
 	
-	public String Interpretation() {
+	public void Interpretation() {
 		this.Imc();
+		if(this.Imc() < 16) {
+			System.out.println("Risque de maladie elevé, maigreur extrême.");
+		}else if(this.Imc() >= 16 && this.Imc()<= 18.4) {
+			System.out.println("Risque de maladie moyen, insuffisance pondérale.");
+		}else if(this.Imc() >= 18.5 && this.Imc()<= 24.9) {
+			System.out.println("Risque de maladie faible, corpulance normale.");
+		}else if(this.Imc() >= 25 && this.Imc()<= 29.9) {
+			System.out.println("Risque de maladie moyen, surpoids.");
+		}else if(this.Imc() >= 30 && this.Imc()<= 34.9) {
+			System.out.println("Risque de maladie élevé, obésité.");
+		}else if(this.Imc() >= 35 && this.Imc()<= 40) {
+			System.out.println("Risque de maladie très élevé, obésité sévère.");
+		}else if(this.Imc() > 40) {
+			System.out.println("Risque de maladie extrêmement elevé, obésité normale.");
 		}
 	}
 }

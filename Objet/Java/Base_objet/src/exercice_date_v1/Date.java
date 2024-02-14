e740d1988527f80d23ebf75531e06456a60df81d
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-02-14 10:03:00 +0100

    exercice date

--- Objet/Java/Base_objet/src/exercice_date_v1/Date.java
+++ Objet/Java/Base_objet/src/exercice_date_v1/Date.java
@@ -18,20 +18,29 @@ public class Date {
 	}
 	
 	private boolean ValiderDate() {
-		//verifie si année est bissextile
-		if(this.annee%4 == 0 && this.annee%100 != 0 || this.annee%400 == 0) {
-			if(this.mois == 2 && this.jour >= 1 && this.jour <= 29) {
-				this.estValide = true;
-				return true;
-			}else if(this.mois != 2 && this.mois >=1 && this.mois <= 7) {
-				if(this.mois%2 == 0) {
-					//
-				}
-			}
-			return true;
-		}else {
-			return false;
+		boolean estBissextile = this.annee%4 == 0 && this.annee%100 != 0 || this.annee%400 == 0;
+		boolean fevrier29 = this.mois == 2 && this.jour >= 1 && this.jour <= 29;
+		boolean fevrier28 = this.mois == 2 && this.jour >= 1 && this.jour <= 28;
+		
+		//valide mois de février à 29 jours années bissextile et mois de février à 28 jours pour années non bissextile
+		if(estBissextile && fevrier29 || !estBissextile && fevrier28) {
+			this.estValide = true;
+		}else{
+			this.estValide = false;
 		}
+		
+		boolean semestre1 = this.mois != 2 && this.mois >=1 && this.mois <= 7 && this.mois%2 == 0 && this.jour >= 1 && this.jour <= 30 || this.mois%2 != 0 && this.jour >= 1 && this.jour <= 31;
+		boolean semestre2 = this.mois != 2 && this.mois >=8 && this.mois <= 12 && this.mois%2 == 0 && this.jour >= 1 && this.jour <= 31 || this.mois%2 != 0 && this.jour >= 1 && this.jour <= 30;
+			
+		//valide les mois de 30 jours et de 31 jours de janvier à juillet
+		if(semestre1) {
+			this.estValide = true;
+		}
+		//valide les mois de 30 jours et de 31 jours de août à décembre
+		if(semestre2) {
+			this.estValide = true;
+		}
+		return this.estValide;
 	}
 	
 	public static String Afficher(Date _date) {

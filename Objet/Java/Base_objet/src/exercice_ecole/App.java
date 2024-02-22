0a5ccb79b22fde106f5527a476489959c21f542b
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-02-22 16:00:09 +0100

    exercice ecole

--- Objet/Java/Base_objet/src/exercice_ecole/App.java
+++ Objet/Java/Base_objet/src/exercice_ecole/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		Professeur prof1 = new Professeur("Brown", "Emmett", 1985, "mathématique");
+		Professeur prof1 = new Professeur("Brown", "Emmett", 1985, "Mathématique");
 		Etudiant etudiant1 = new Etudiant("McFly", "Marty", 2015, "Bac S");
 		
 		System.out.println(etudiant1.DireBonjour());

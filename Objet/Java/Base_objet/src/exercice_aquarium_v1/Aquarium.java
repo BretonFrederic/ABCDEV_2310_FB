9382fc09ccaba2efc7f3bfc54f4377a36551e052
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-02-27 16:46:51 +0100

    exercice aquarium

---
+++ Objet/Java/Base_objet/src/exercice_aquarium_v1/Aquarium.java
@@ -0,0 +1,23 @@
+package exercice_aquarium_v1;
+
+import java.util.ArrayList;
+
+public class Aquarium {
+
+	//attributs
+	private boolean estRempli;
+	private int nbTours;
+	private ArrayList<Algue> algue;
+			
+	//constructeur
+	public Aquarium() {
+		
+	}
+			
+	//getters et setters
+			
+			
+	//méthodes
+		
+	
+}
--- Objet/Java/Base_objet/src/exercice_aquarium_v1/PoissonHerbivore.java
+++ Objet/Java/Base_objet/src/exercice_aquarium_v1/PoissonHerbivore.java
@@ -3,17 +3,17 @@ package exercice_aquarium_v1;
 public class PoissonHerbivore extends Poisson {
 	
 	//attributs
-		private RaceH race;
+	private RaceH race;
 		
-		//constructeur
-		public PoissonHerbivore(String _nom, String _sexe, RaceH _race) {
-			super(_nom, _sexe);
-			this.race = _race;
-		}
+	//constructeur
+	public PoissonHerbivore(String _nom, String _sexe, RaceH _race) {
+		super(_nom, _sexe);
+		this.race = _race;
+	}
 		
-		//getters et setters
+	//getters et setters
 		
 		
-		//méthodes
+	//méthodes
 	
 }

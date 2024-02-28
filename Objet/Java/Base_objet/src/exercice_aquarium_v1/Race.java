1a7f20b9e25a25c4c1aa296e59d58cde349667ad
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-02-28 16:54:59 +0100

    exercice javaquarium

--- Objet/Java/Base_objet/src/exercice_aquarium_v1/Race.java
+++ Objet/Java/Base_objet/src/exercice_aquarium_v1/Race.java
@@ -2,5 +2,15 @@ package exercice_aquarium_v1;
 
 public enum Race {
 
-	SOLE, BAR, CARPE, MEROU, THON, POISSON_CLOWN;
+	SOLE("sole"), BAR("bar"), CARPE("carpe"), MEROU("mérou"), THON("thon"), POISSON_CLOWN("poisson-clown");
+	
+	//attributs
+	private final String poisson;
+	
+	//constructeur
+	public Race(String _poisson){
+		this.poisson = _poisson;
+	}
+	
+	//getters et setters
 }
